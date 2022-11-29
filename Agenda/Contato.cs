a1c20bc429f7291e87d585b073a9faa437340e8d
Caio Furegati <caiofu@hotmail.com>
2022-11-29 15:48:12 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda/Contato.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    internal class Contato
+    {
+        int id;
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        DateTime? dataNascimento { get; set; }
+
+
+        //METODOS CONSTRUTORES
+        public Contato(string nome, string telefone, DateTime dataNascimento)
+        {
+
+            this.Nome = nome;
+            this.Telefone = telefone;
+        }
+
+        public Contato(string nome, string telefone)
+        {
+            this.Nome = nome;
+            this.Telefone = telefone;
+        }
+
+        //METODOS
+        private void CriarContato()
+        {
+
+        }
+    }
+}
---
+++ Agenda/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Agenda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contato> ListaContatos = new List<Contato>();
+            ListaContatos.Add(new Contato(Console.ReadLine(), Console.ReadLine()));
+
+            foreach (var Contato in ListaContatos)
+            {
+                Console.WriteLine("Nome: {0} Telefone: {1}", Contato.Nome, Contato.Telefone);
+
+            }
+        }
+    }
+}
